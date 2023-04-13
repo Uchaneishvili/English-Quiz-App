c1f6377024765e2a4567809c452bacd30ab1412a
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2023-04-14 01:23:13 +0200

    fix

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,151 +1,71 @@
 /** @format */
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Modal, Button } from 'antd';
 import styles from './Quiz.module.css';
 
-function Quiz() {
-	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-	const [selectedAnswer, setSelectedAnswer] = useState(null);
-	const [showResult, setShowResult] = useState(false);
-	const [score, setScore] = useState(0);
-
+function Quiz(props) {
 	const handleAnswerSelect = (answer) => {
-		setSelectedAnswer(answer);
+		props.setSelectedAnswer(answer);
 	};
 
-	const handleNextQuestion = () => {
-		const currentQuestion = questions[currentQuestionIndex];
-		if (currentQuestion.answer === selectedAnswer) {
-			setScore(score + 1);
-		}
-		setSelectedAnswer(null);
-		setCurrentQuestionIndex(currentQuestionIndex + 1);
+	const handleSubmitAnswer = () => {
+		props.handleNextQuestion();
+		props.setEnabled(false);
 	};
-
-	const handleShowResult = () => {
-		const currentQuestion = questions[currentQuestionIndex];
-		if (currentQuestion.answer === selectedAnswer) {
-			setScore(score + 1);
-		}
-		setShowResult(true);
-	};
-
-	const handleRestart = () => {
-		setCurrentQuestionIndex(0);
-		setSelectedAnswer(null);
-		setShowResult(false);
-		setScore(0);
-	};
-
-	const questions = [
-		{
-			question:
-				'Which of the following is a type of sentence that gives a command or makes a request?',
-			options: ['Interrogative', 'Exclamatory', 'Imperative', 'Declarative'],
-			answer: 'Imperative',
-		},
-		{
-			question:
-				"What is the correct spelling of the word that means 'extremely beautiful'?",
-			options: ['Gorgeous', 'Gorgous', 'Gorgeus', 'Gorgius'],
-			answer: 'Gorgeous',
-		},
-		{
-			question:
-				"Which of the following is a type of figurative language that compares two unlike things using 'like' or 'as'?",
-			options: ['Metaphor', 'Simile', 'Personification', 'Hyperbole'],
-			answer: 'Simile',
-		},
-		{
-			question:
-				'What is the name for a word that has the same or nearly the same meaning as another word?',
-			options: ['Antonym', 'Synonym', 'Homonym', 'Heteronym'],
-			answer: 'Synonym',
-		},
-		{
-			question: "What is the plural of the word 'goose'?",
-			options: ['Geese', 'Gooses', 'Geeses', 'Gice'],
-			answer: 'Geese',
-		},
-		{
-			question:
-				'Which of the following is a type of sentence that expresses strong emotion?',
-			options: ['Interrogative', 'Exclamatory', 'Imperative', 'Declarative'],
-			answer: 'Exclamatory',
-		},
-		{
-			question:
-				"What is the correct spelling of the word that means 'the act of making something less severe'?",
-			options: ['Mitigation', 'Mitagation', 'Mittigation', 'Mittagation'],
-			answer: 'Mitigation',
-		},
-		{
-			question:
-				'Which of the following is a type of figurative language that gives human qualities to non-human things?',
-			options: ['Metaphor', 'Simile', 'Personification', 'Hyperbole'],
-			answer: 'Personification',
-		},
-		{
-			question:
-				'What is the name for a word that is spelled the same as another word, but has a different meaning?',
-			options: ['Antonym', 'Synonym', 'Homonym', 'Heteronym'],
-			answer: 'Homonym',
-		},
-	];
-
-	const currentQuestion = questions[currentQuestionIndex];
 
 	return (
 		<Modal
-			open={true}
+			open={props.enabled}
 			closable={false}
-			title={<h2>{currentQuestion.question}</h2>}
+			title={<h2>{!props.showResult && props.currentQuestion.question}</h2>}
 			footer={
 				<>
-					{currentQuestionIndex < questions.length - 1 ? (
-						<Button
-							type='primary'
-							disabled={!selectedAnswer}
-							onClick={handleNextQuestion}>
-							Next Question
-						</Button>
+					{props.currentQuestionIndex < props.questions.length - 1 ? (
+						<>
+							<Button
+								type='primary'
+								disabled={!props.selectedAnswer}
+								onClick={handleSubmitAnswer}>
+								Submit Answer
+							</Button>
+						</>
 					) : (
 						<div>
-							{!showResult && (
+							{!props.showResult && (
 								<Button
 									type='primary'
-									disabled={!selectedAnswer}
-									onClick={handleShowResult}>
+									disabled={!props.selectedAnswer}
+									onClick={props.handleShowResult}>
 									Show Result
 								</Button>
 							)}
 						</div>
 					)}
 
-					{showResult && (
+					{props.showResult && (
 						<Button
 							type='primary'
-							onClick={handleRestart}>
+							onClick={() => props.handleRestart()}>
 							Restart Quiz
 						</Button>
 					)}
 				</>
 			}>
-			{showResult ? (
+			{props.showResult ? (
 				<div>
 					<h2>
-						Your score: {score}/{questions.length}
+						Your score: {props.score}/{props.questions.length}
 					</h2>
 				</div>
 			) : (
 				<div>
 					<div className={styles.container}>
-						{currentQuestion.options.map((option) => (
+						{props.currentQuestion.options.map((option) => (
 							<Button
 								className={styles.button}
 								key={option}
-								type={selectedAnswer === option ? 'primary' : 'default'}
+								type={props.selectedAnswer === option ? 'primary' : 'default'}
 								onClick={() => handleAnswerSelect(option)}>
 								{option}
 							</Button>
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,21 +1,125 @@
 /** @format */
 
-import React, { useState } from "react";
-import Quiz from "../components/Quiz";
-import Dice from "../components/Dice";
-import { Button } from "antd";
+import React, { useState } from 'react';
+import Quiz from '../components/Quiz';
+import Dice from '../components/Dice';
+import { Button } from 'antd';
 
 function MainPage() {
-  const [isEnabled, setIsEnabled] = useState(false);
+	const [isEnabled, setIsEnabled] = useState(false);
+	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+	const [score, setScore] = useState(0);
+	const [showResult, setShowResult] = useState(false);
+	const [selectedAnswer, setSelectedAnswer] = useState(null);
 
-  return (
-    <div>
-      <Dice />
-      <Button onClick={() => setIsEnabled(true)}>Press Me!</Button>
+	const questions = [
+		{
+			question:
+				'Which of the following is a type of sentence that gives a command or makes a request?',
+			options: ['Interrogative', 'Exclamatory', 'Imperative', 'Declarative'],
+			answer: 'Imperative',
+		},
+		// {
+		// 	question:
+		// 		"What is the correct spelling of the word that means 'extremely beautiful'?",
+		// 	options: ['Gorgeous', 'Gorgous', 'Gorgeus', 'Gorgius'],
+		// 	answer: 'Gorgeous',
+		// },
+		// {
+		// 	question:
+		// 		"Which of the following is a type of figurative language that compares two unlike things using 'like' or 'as'?",
+		// 	options: ['Metaphor', 'Simile', 'Personification', 'Hyperbole'],
+		// 	answer: 'Simile',
+		// },
+		// {
+		// 	question:
+		// 		'What is the name for a word that has the same or nearly the same meaning as another word?',
+		// 	options: ['Antonym', 'Synonym', 'Homonym', 'Heteronym'],
+		// 	answer: 'Synonym',
+		// },
+		// {
+		// 	question: "What is the plural of the word 'goose'?",
+		// 	options: ['Geese', 'Gooses', 'Geeses', 'Gice'],
+		// 	answer: 'Geese',
+		// },
+		// {
+		// 	question:
+		// 		'Which of the following is a type of sentence that expresses strong emotion?',
+		// 	options: ['Interrogative', 'Exclamatory', 'Imperative', 'Declarative'],
+		// 	answer: 'Exclamatory',
+		// },
+		// {
+		// 	question:
+		// 		"What is the correct spelling of the word that means 'the act of making something less severe'?",
+		// 	options: ['Mitigation', 'Mitagation', 'Mittigation', 'Mittagation'],
+		// 	answer: 'Mitigation',
+		// },
+		// {
+		// 	question:
+		// 		'Which of the following is a type of figurative language that gives human qualities to non-human things?',
+		// 	options: ['Metaphor', 'Simile', 'Personification', 'Hyperbole'],
+		// 	answer: 'Personification',
+		// },
+		// {
+		// 	question:
+		// 		'What is the name for a word that is spelled the same as another word, but has a different meaning?',
+		// 	options: ['Antonym', 'Synonym', 'Homonym', 'Heteronym'],
+		// 	answer: 'Homonym',
+		// },
+	];
 
-      {isEnabled && <Quiz />}
-    </div>
-  );
+	const currentQuestion = questions[currentQuestionIndex];
+
+	const handleNextQuestion = () => {
+		const currentQuestion = questions[currentQuestionIndex];
+
+		if (currentQuestion.answer === selectedAnswer) {
+			setScore(score + 1);
+		}
+		setCurrentQuestionIndex(currentQuestionIndex + 1);
+		setShowResult(false);
+	};
+
+	const handleShowResult = () => {
+		setShowResult(true);
+
+		const currentQuestion = questions[currentQuestionIndex];
+		if (currentQuestion.answer === selectedAnswer) {
+			setScore(score + 1);
+		}
+	};
+
+	const handleRestart = () => {
+		setCurrentQuestionIndex(0);
+		setScore(0);
+		setShowResult(false);
+		setSelectedAnswer(false);
+		setIsEnabled(false);
+	};
+
+	return (
+		<div>
+			<Dice />
+			<Button onClick={() => setIsEnabled(true)}>Press Me!</Button>
+
+			{isEnabled && (
+				<Quiz
+					enabled={isEnabled}
+					setEnabled={setIsEnabled}
+					handleShowResult={handleShowResult}
+					handleNextQuestion={handleNextQuestion}
+					handleRestart={handleRestart}
+					currentQuestion={currentQuestion}
+					questions={questions}
+					showResult={showResult}
+					score={score}
+					selectedAnswer={selectedAnswer}
+					setSelectedAnswer={setSelectedAnswer}
+					currentQuestionIndex={currentQuestionIndex}
+				/>
+			)}
+		</div>
+	);
 }
 
 export default MainPage;
