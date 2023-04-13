5c3429810c3ba1cfadb77381472b7ea7381ad3e3
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2023-04-13 23:34:39 +0200

    initial commit

---
+++ src/components/Quiz.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from 'react';
+
+function Quiz() {
+	return <div>Quiz</div>;
+}
+
+export default Quiz;
---
+++ src/pages/MainPage.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from 'react';
+
+import { Table, Button } from 'antd';
+
+function MainPage() {
+	return (
+		<div>
+			<Table />
+			<Button>125</Button>
+		</div>
+	);
+}
+
+export default MainPage;
