342b6578796e9bb0397c3ba4589e1e364dc8ac43
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2023-04-14 15:39:55 +0200

    fix

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,175 +1,170 @@
 /** @format */
 
-import React from "react";
-import { useState } from "react";
-import { Modal, Button } from "antd";
-import styles from "./Quiz.module.css";
+import React from 'react';
+import { useState } from 'react';
+import { Modal, Button } from 'antd';
+import styles from './Quiz.module.css';
 
 function Quiz(props) {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [score, setScore] = useState(0);
-  const [showResult, setShowResult] = useState(false);
-  const [selectedAnswer, setSelectedAnswer] = useState(null);
+	const [score, setScore] = useState(0);
+	const [showResult, setShowResult] = useState(false);
+	const [selectedAnswer, setSelectedAnswer] = useState(null);
 
-  const questions = [
-    {
-      question:
-        "Which of the following is a type of sentence that gives a command or makes a request?",
-      options: ["Interrogative", "Exclamatory", "Imperative", "Declarative"],
-      answer: "Imperative",
-    },
-    {
-      question:
-        "What is the correct spelling of the word that means 'extremely beautiful'?",
-      options: ["Gorgeous", "Gorgous", "Gorgeus", "Gorgius"],
-      answer: "Gorgeous",
-    },
-    {
-      question:
-        "Which of the following is a type of figurative language that compares two unlike things using 'like' or 'as'?",
-      options: ["Metaphor", "Simile", "Personification", "Hyperbole"],
-      answer: "Simile",
-    },
-    {
-      question:
-        "What is the name for a word that has the same or nearly the same meaning as another word?",
-      options: ["Antonym", "Synonym", "Homonym", "Heteronym"],
-      answer: "Synonym",
-    },
-    {
-      question: "What is the plural of the word 'goose'?",
-      options: ["Geese", "Gooses", "Geeses", "Gice"],
-      answer: "Geese",
-    },
-    {
-      question:
-        "Which of the following is a type of sentence that expresses strong emotion?",
-      options: ["Interrogative", "Exclamatory", "Imperative", "Declarative"],
-      answer: "Exclamatory",
-    },
-    {
-      question:
-        "What is the correct spelling of the word that means 'the act of making something less severe'?",
-      options: ["Mitigation", "Mitagation", "Mittigation", "Mittagation"],
-      answer: "Mitigation",
-    },
-    {
-      question:
-        "Which of the following is a type of figurative language that gives human qualities to non-human things?",
-      options: ["Metaphor", "Simile", "Personification", "Hyperbole"],
-      answer: "Personification",
-    },
-    {
-      question:
-        "What is the name for a word that is spelled the same as another word, but has a different meaning?",
-      options: ["Antonym", "Synonym", "Homonym", "Heteronym"],
-      answer: "Homonym",
-    },
-  ];
+	const questions = [
+		{
+			question:
+				'Which of the following is a type of sentence that gives a command or makes a request?',
+			options: ['Interrogative', 'Exclamatory', 'Imperative', 'Declarative'],
+			answer: 'Imperative',
+		},
+		{
+			question:
+				"What is the correct spelling of the word that means 'extremely beautiful'?",
+			options: ['Gorgeous', 'Gorgous', 'Gorgeus', 'Gorgius'],
+			answer: 'Gorgeous',
+		},
+		// {
+		// 	question:
+		// 		"Which of the following is a type of figurative language that compares two unlike things using 'like' or 'as'?",
+		// 	options: ['Metaphor', 'Simile', 'Personification', 'Hyperbole'],
+		// 	answer: 'Simile',
+		// },
+		// {
+		// 	question:
+		// 		'What is the name for a word that has the same or nearly the same meaning as another word?',
+		// 	options: ['Antonym', 'Synonym', 'Homonym', 'Heteronym'],
+		// 	answer: 'Synonym',
+		// },
+		// {
+		// 	question: "What is the plural of the word 'goose'?",
+		// 	options: ['Geese', 'Gooses', 'Geeses', 'Gice'],
+		// 	answer: 'Geese',
+		// },
+		// {
+		// 	question:
+		// 		'Which of the following is a type of sentence that expresses strong emotion?',
+		// 	options: ['Interrogative', 'Exclamatory', 'Imperative', 'Declarative'],
+		// 	answer: 'Exclamatory',
+		// },
+		// {
+		// 	question:
+		// 		"What is the correct spelling of the word that means 'the act of making something less severe'?",
+		// 	options: ['Mitigation', 'Mitagation', 'Mittigation', 'Mittagation'],
+		// 	answer: 'Mitigation',
+		// },
+		// {
+		// 	question:
+		// 		'Which of the following is a type of figurative language that gives human qualities to non-human things?',
+		// 	options: ['Metaphor', 'Simile', 'Personification', 'Hyperbole'],
+		// 	answer: 'Personification',
+		// },
+		// {
+		// 	question:
+		// 		'What is the name for a word that is spelled the same as another word, but has a different meaning?',
+		// 	options: ['Antonym', 'Synonym', 'Homonym', 'Heteronym'],
+		// 	answer: 'Homonym',
+		// },
+	];
 
-  const currentQuestion = questions[currentQuestionIndex];
+	const currentQuestion = questions[props.currentQuestionIndex];
 
-  const handleNextQuestion = () => {
-    const currentQuestion = questions[currentQuestionIndex];
+	const handleNextQuestion = () => {
+		const currentQuestion = questions[props.currentQuestionIndex];
 
-    if (currentQuestion.answer === selectedAnswer) {
-      setScore(score + 1);
-    }
+		if (currentQuestion.answer === selectedAnswer) {
+			setScore(score + 1);
+		}
 
-    setCurrentQuestionIndex(currentQuestionIndex + 1);
-    setShowResult(false);
+		props.setCurrentQuestionIndex(props.currentQuestionIndex + 1);
+		setShowResult(false);
+	};
 
-    console.log(currentQuestionIndex);
-  };
+	const handleShowResult = () => {
+		setShowResult(true);
 
-  const handleShowResult = () => {
-    setShowResult(true);
+		const currentQuestion = questions[props.currentQuestionIndex];
+		if (currentQuestion.answer === selectedAnswer) {
+			setScore(score + 1);
+		}
+	};
 
-    const currentQuestion = questions[currentQuestionIndex];
-    if (currentQuestion.answer === selectedAnswer) {
-      setScore(score + 1);
-    }
-  };
+	const handleRestart = () => {
+		props.setCurrentQuestionIndex(0);
+		setScore(0);
+		setShowResult(false);
+		setSelectedAnswer(false);
+		props.setEnabled(false);
+	};
 
-  const handleRestart = () => {
-    setCurrentQuestionIndex(0);
-    setScore(0);
-    setShowResult(false);
-    setSelectedAnswer(false);
-    props.setIsEnabled(false);
-  };
+	const handleAnswerSelect = (answer) => {
+		setSelectedAnswer(answer);
+	};
 
-  const handleAnswerSelect = (answer) => {
-    setSelectedAnswer(answer);
-  };
+	const handleSubmitAnswer = () => {
+		handleNextQuestion();
+		props.setEnabled(false);
+	};
+	return (
+		<Modal
+			open={props.enabled}
+			closable={false}
+			title={<h2>{!showResult && currentQuestion.question}</h2>}
+			footer={
+				<>
+					{props.currentQuestionIndex < questions.length - 1 ? (
+						<>
+							<Button
+								type='primary'
+								disabled={!selectedAnswer}
+								onClick={handleSubmitAnswer}>
+								Submit Answer
+							</Button>
+						</>
+					) : (
+						<div>
+							{!showResult && (
+								<Button
+									type='primary'
+									disabled={!selectedAnswer}
+									onClick={handleShowResult}>
+									Show Result
+								</Button>
+							)}
+						</div>
+					)}
 
-  const handleSubmitAnswer = () => {
-    handleNextQuestion();
-    props.setEnabled(false);
-  };
-  return (
-    <Modal
-      open={props.enabled}
-      closable={false}
-      title={<h2>{!showResult && currentQuestion.question}</h2>}
-      footer={
-        <>
-          {currentQuestionIndex < questions.length - 1 ? (
-            <>
-              <Button
-                type="primary"
-                disabled={!selectedAnswer}
-                onClick={handleSubmitAnswer}
-              >
-                Submit Answer
-              </Button>
-            </>
-          ) : (
-            <div>
-              {!showResult && (
-                <Button
-                  type="primary"
-                  disabled={!selectedAnswer}
-                  onClick={handleShowResult}
-                >
-                  Show Result
-                </Button>
-              )}
-            </div>
-          )}
-
-          {showResult && (
-            <Button type="primary" onClick={() => handleRestart()}>
-              Restart Quiz
-            </Button>
-          )}
-        </>
-      }
-    >
-      {showResult ? (
-        <div>
-          <h2>
-            Your score: {score}/{questions.length}
-          </h2>
-        </div>
-      ) : (
-        <div>
-          <div className={styles.container}>
-            {currentQuestion.options.map((option) => (
-              <Button
-                className={styles.button}
-                key={option}
-                type={selectedAnswer === option ? "primary" : "default"}
-                onClick={() => handleAnswerSelect(option)}
-              >
-                {option}
-              </Button>
-            ))}
-          </div>
-        </div>
-      )}
-    </Modal>
-  );
+					{showResult && (
+						<Button
+							type='primary'
+							onClick={() => handleRestart()}>
+							Restart Quiz
+						</Button>
+					)}
+				</>
+			}>
+			{showResult ? (
+				<div>
+					<h2>
+						Your score: {score}/{questions.length}
+					</h2>
+				</div>
+			) : (
+				<div>
+					<div className={styles.container}>
+						{currentQuestion.options.map((option) => (
+							<Button
+								className={styles.button}
+								key={option}
+								type={selectedAnswer === option ? 'primary' : 'default'}
+								onClick={() => handleAnswerSelect(option)}>
+								{option}
+							</Button>
+						))}
+					</div>
+				</div>
+			)}
+		</Modal>
+	);
 }
 
 export default Quiz;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,23 +1,30 @@
 /** @format */
 
-import React, { useState } from "react";
-import Quiz from "../components/Quiz";
-import Dice from "../components/Dice";
+import React, { useState } from 'react';
+import Quiz from '../components/Quiz';
+import Dice from '../components/Dice';
 
 function MainPage() {
-  const [isEnabled, setIsEnabled] = useState(false);
+	const [isEnabled, setIsEnabled] = useState(false);
+	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
-  const showQuizzHandler = (show) => {
-    setIsEnabled(show);
-  };
+	const showQuizzHandler = (show) => {
+		setIsEnabled(show);
+	};
 
- 
-  return (
-    <div>
-      <Dice onShowQuizz={showQuizzHandler} />
-      {isEnabled && <Quiz enabled={isEnabled} setEnabled={setIsEnabled} />}
-    </div>
-  );
+	return (
+		<div>
+			<Dice onShowQuizz={showQuizzHandler} />
+			{isEnabled && (
+				<Quiz
+					enabled={isEnabled}
+					setEnabled={setIsEnabled}
+					setCurrentQuestionIndex={setCurrentQuestionIndex}
+					currentQuestionIndex={currentQuestionIndex}
+				/>
+			)}
+		</div>
+	);
 }
 
 export default MainPage;
