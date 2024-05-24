c8a2a6c6f47eec133ab06c57c42c44cac0e64a69
Giga Uchaneishvili <g.uchaneishvili@tnet.loc>
2024-05-25 02:11:31 +0400

    fix responsive

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -16,66 +16,66 @@ import { addNumber } from "../util/reduxStore";
 import sound from "../Audio/rollingDice.mp3";
 
 const Dice = (props) => {
-  const play = () => {
-    new Audio(sound).play();
-  };
-  const numbers = useSelector((state) => state.numbers);
-  const dispatch = useDispatch();
-  const ref = useRef();
+	const play = () => {
+		new Audio(sound).play();
+	};
+	const numbers = useSelector((state) => state.numbers);
+	const dispatch = useDispatch();
+	const ref = useRef();
 
-  const gnrt = () => {
-    let num;
-    do {
-      num = Math.floor(Math.random() * 6 + 1);
-    } while (num === numbers[numbers.length - 1]);
-    return num;
-  };
-  const rollHandler = () => {
-    play();
-    var rndm = gnrt();
+	const gnrt = () => {
+		let num;
+		do {
+			num = Math.floor(Math.random() * 6 + 1);
+		} while (num === numbers[numbers.length - 1]);
+		return num;
+	};
+	const rollHandler = () => {
+		play();
+		var rndm = gnrt();
 
-    dispatch(addNumber(rndm));
+		dispatch(addNumber(rndm));
 
-    let showClass = "show" + rndm;
-    var currentClass = ref.current.classList;
-    if (currentClass !== "cube") {
-      ref.current.classList = "cube";
-    }
-    ref.current.classList.add(showClass);
+		let showClass = "show" + rndm;
+		var currentClass = ref.current.classList;
+		if (currentClass !== "cube") {
+			ref.current.classList = "cube";
+		}
+		ref.current.classList.add(showClass);
 
-    setTimeout(() => {
-      props.onShowQuizz(true);
-    }, 1000);
-  };
+		setTimeout(() => {
+			props.onShowQuizz(true);
+		}, 1000);
+	};
 
-  return (
-    <div className="scene">
-      <div className="cube" ref={ref}>
-        <div className={`cube__face cube__face1`}>
-          <img src={cubeImg1} alt=""></img>
-        </div>
-        <div className={`cube__face cube__face2`}>
-          <img src={cubeImg2} alt=""></img>
-        </div>
-        <div className={`cube__face cube__face3`}>
-          <img src={cubeImg3} alt=""></img>
-        </div>
-        <div className={`cube__face cube__face4`}>
-          <img src={cubeImg4} alt=""></img>
-        </div>
-        <div className={`cube__face cube__face5`}>
-          <img src={cubeImg5} alt=""></img>
-        </div>
-        <div className={`cube__face cube__face6`}>
-          <img src={cubeImg6} alt=""></img>
-        </div>
-      </div>
+	return (
+		<div className="scene">
+			<div className="cube" ref={ref}>
+				<div className={`cube__face cube__face1`}>
+					<img src={cubeImg1} alt=""></img>
+				</div>
+				<div className={`cube__face cube__face2`}>
+					<img src={cubeImg2} alt=""></img>
+				</div>
+				<div className={`cube__face cube__face3`}>
+					<img src={cubeImg3} alt=""></img>
+				</div>
+				<div className={`cube__face cube__face4`}>
+					<img src={cubeImg4} alt=""></img>
+				</div>
+				<div className={`cube__face cube__face5`}>
+					<img src={cubeImg5} alt=""></img>
+				</div>
+				<div className={`cube__face cube__face6`}>
+					<img src={cubeImg6} alt=""></img>
+				</div>
+			</div>
 
-      <Button onClick={rollHandler} className="btn-roll">
-        Roll Dice
-      </Button>
-    </div>
-  );
+			<Button onClick={rollHandler} className="btn-roll">
+				Roll Dice
+			</Button>
+		</div>
+	);
 };
 
 export default Dice;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -13,190 +13,198 @@ import audio from "../Audio/win.mp3";
 import startAudio from "../Audio/click.mp3";
 
 function MainPage() {
-  const dispatch = useDispatch();
-  const [isEnabled, setIsEnabled] = useState(false);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const numbers = useSelector((state) => state.numbers);
-  const activePlayer = useSelector((state) => state.activePlayer);
-  const [oneScore, setOneScore] = useState(0);
-  const [twoScore, setTwoScore] = useState(0);
-  const [startG, setStartG] = useState(false);
-  const [isWinner, setIsWinner] = useState(0);
-  const [isFinished, setIsFinished] = useState();
-  const [fPlayerName, setFirstPlayerName] = useState("Player1");
-  const [sPlayerName, setSecondPlayerName] = useState("Player2");
-  const [showAlert, setShowAlert] = useState(false);
+	const dispatch = useDispatch();
+	const [isEnabled, setIsEnabled] = useState(false);
+	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+	const numbers = useSelector((state) => state.numbers);
+	const activePlayer = useSelector((state) => state.activePlayer);
+	const [oneScore, setOneScore] = useState(0);
+	const [twoScore, setTwoScore] = useState(0);
+	const [startG, setStartG] = useState(false);
+	const [isWinner, setIsWinner] = useState(0);
+	const [isFinished, setIsFinished] = useState();
+	const [fPlayerName, setFirstPlayerName] = useState("Player1");
+	const [sPlayerName, setSecondPlayerName] = useState("Player2");
+	const [showAlert, setShowAlert] = useState(false);
 
-  const startAudioHandler = () => {
-    new Audio(startAudio).play();
-  };
-  const winAudio = () => {
-    new Audio(audio).play();
-  };
-  const setFnameHandler = (e) => {
-    setFirstPlayerName(e.target.value);
-  };
-  const setSnameHandler = (e) => {
-    setSecondPlayerName(e.target.value);
-  };
+	const startAudioHandler = () => {
+		new Audio(startAudio).play();
+	};
+	const winAudio = () => {
+		new Audio(audio).play();
+	};
+	const setFnameHandler = (e) => {
+		setFirstPlayerName(e.target.value);
+	};
+	const setSnameHandler = (e) => {
+		setSecondPlayerName(e.target.value);
+	};
 
-  const showQuizzHandler = (show) => {
-    if (numbers.length === 1) {
-      setIsEnabled(show);
-    }
-  };
+	const showQuizzHandler = (show) => {
+		if (numbers.length === 1) {
+			setIsEnabled(show);
+		}
+	};
 
-  useEffect(() => {
-    if (isEnabled === false) {
-      dispatch(resetNumbers());
-    }
-  }, [isEnabled, dispatch]);
+	useEffect(() => {
+		if (isEnabled === false) {
+			dispatch(resetNumbers());
+		}
+	}, [isEnabled, dispatch]);
 
-  useEffect(() => {
-    if (numbers.length > 1) {
-      if (numbers[numbers.length - 1] < numbers[numbers.length - 2]) {
-        dispatch(setActivePlayer(1));
-      } else if (numbers[numbers.length - 1] > numbers[numbers.length - 2]) {
-        dispatch(setActivePlayer(2));
-      }
-    }
-  }, [numbers.length, numbers, dispatch]);
-  const gameStartHandler = () => {
-    if (fPlayerName !== "Player1" && sPlayerName !== "Player2") {
-      setStartG(true);
-    } else {
-      setShowAlert(true);
-    }
-  };
+	useEffect(() => {
+		if (numbers.length > 1) {
+			if (numbers[numbers.length - 1] < numbers[numbers.length - 2]) {
+				dispatch(setActivePlayer(1));
+			} else if (numbers[numbers.length - 1] > numbers[numbers.length - 2]) {
+				dispatch(setActivePlayer(2));
+			}
+		}
+	}, [numbers.length, numbers, dispatch]);
+	const gameStartHandler = () => {
+		if (fPlayerName !== "Player1" && sPlayerName !== "Player2") {
+			setStartG(true);
+		} else {
+			setShowAlert(true);
+		}
+	};
 
-  useEffect(() => {
-    if (isFinished) {
-      if (oneScore > twoScore) {
-        setIsWinner(1);
-      } else if (oneScore < twoScore) {
-        setIsWinner(2);
-      } else {
-        setIsWinner(3);
-      }
-    }
-  }, [isFinished, oneScore, twoScore]);
+	useEffect(() => {
+		if (isFinished) {
+			if (oneScore > twoScore) {
+				setIsWinner(1);
+			} else if (oneScore < twoScore) {
+				setIsWinner(2);
+			} else {
+				setIsWinner(3);
+			}
+		}
+	}, [isFinished, oneScore, twoScore]);
 
-  return (
-    <>
-      <div className={styles.container}>
-        <div
-          className={
-            activePlayer === 1 ? styles.activePlayerSide : styles.playerSide
-          }
-        >
-          <div className={styles.playerName}>
-            <div
-              className={
-                isWinner === 1 && isWinner !== 0 && isWinner !== 2
-                  ? styles.firstTrophy
-                  : styles.hide
-              }
-            >
-              <TrophyFilled />
-            </div>
-            <h2> {fPlayerName} </h2>
-            {activePlayer === 1 && (
-              <p
-                style={{
-                  display: "flex",
-                  position: "absolute",
-                  top: "36%",
-                  left: "31%",
-                }}
-              ></p>
-            )}
-          </div>
-          <div className={styles.score}>{oneScore}</div>
-        </div>
-        <div className={styles.middle}>
-          <div style={!startG ? { display: "none" } : { display: "flex" }}>
-            <Dice onShowQuizz={showQuizzHandler} />
-          </div>
-          <div
-            className={startG ? styles.hide : styles.btnContainer}
-            onClick={startAudioHandler}
-          >
-            <Button onClick={gameStartHandler} className={styles.btnRoll}>
-              <PlayCircleFilled />
-              Play
-            </Button>
-            <div className={styles.inputContainer}>
-              <Input placeholder="Player 1 Name" onChange={setFnameHandler} />
-              <Input placeholder="Player 2 Name" onChange={setSnameHandler} />
-            </div>
+	useEffect(() => {
+		setInterval(() => {
+			if (isWinner) {
+				window.location.reload();
+			}
+		}, 1000);
+	}, [isWinner]);
 
-            {showAlert && (
-              <Space
-                direction="vertical"
-                style={{ width: "100%", position: "relative" }}
-              >
-                <Alert
-                  message="Players' Names Are Required"
-                  type="warning"
-                  showIcon
-                  closable
-                  onClick={() => {
-                    setShowAlert(false);
-                  }}
-                  style={{ position: "absolute", top: "20px" }}
-                />
-              </Space>
-            )}
-          </div>
-          {isEnabled && (
-            <Quiz
-              setIsFinished={setIsFinished}
-              setIsWinner={setIsWinner}
-              setOneScore={setOneScore}
-              oneScore={oneScore}
-              setTwoScore={setTwoScore}
-              twoScore={twoScore}
-              enabled={isEnabled}
-              setEnabled={setIsEnabled}
-              setCurrentQuestionIndex={setCurrentQuestionIndex}
-              currentQuestionIndex={currentQuestionIndex}
-              fPlayerName={fPlayerName}
-              sPlayerName={sPlayerName}
-            />
-          )}
-        </div>
-        <div
-          className={
-            activePlayer === 2 ? styles.activePlayerSide : styles.playerSide
-          }
-        >
-          <div className={styles.playerName}>
-            <div
-              className={
-                isWinner === 2 && isWinner !== 0 && isWinner !== 1
-                  ? styles.secondTrophy
-                  : styles.hide
-              }
-            >
-              {isWinner && winAudio()} <TrophyFilled />
-            </div>
-            {activePlayer === 2 && (
-              <p
-                style={{
-                  display: "flex",
-                  position: "absolute",
-                  top: "36%",
-                  right: "17%",
-                }}
-              ></p>
-            )}
-            <h2> {sPlayerName} </h2>
-          </div>
-          <div className={styles.score}>{twoScore}</div>
-        </div>
-      </div>
-      <InitialModal />
-    </>
-  );
+	return (
+		<>
+			<div className={styles.container}>
+				<div
+					className={
+						activePlayer === 1 ? styles.activePlayerSide : styles.playerSide
+					}
+				>
+					<div className={styles.playerName}>
+						<div
+							className={
+								isWinner === 1 && isWinner !== 0 && isWinner !== 2
+									? styles.firstTrophy
+									: styles.hide
+							}
+						>
+							<TrophyFilled />
+						</div>
+						<h2> {fPlayerName} </h2>
+						{activePlayer === 1 && (
+							<p
+								style={{
+									display: "flex",
+									position: "absolute",
+									top: "36%",
+									left: "31%",
+								}}
+							></p>
+						)}
+					</div>
+					<div className={styles.score}>{oneScore}</div>
+				</div>
+				<div className={styles.middle}>
+					<div style={!startG ? { display: "none" } : { display: "flex" }}>
+						<Dice onShowQuizz={showQuizzHandler} />
+					</div>
+					<div
+						className={startG ? styles.hide : styles.btnContainer}
+						onClick={startAudioHandler}
+					>
+						<Button onClick={gameStartHandler} className={styles.btnRoll}>
+							<PlayCircleFilled />
+							Play
+						</Button>
+						<div className={styles.inputContainer}>
+							<Input placeholder="Player 1 Name" onChange={setFnameHandler} />
+							<Input placeholder="Player 2 Name" onChange={setSnameHandler} />
+						</div>
+
+						{showAlert && (
+							<Space
+								direction="vertical"
+								style={{ width: "100%", position: "relative" }}
+							>
+								<Alert
+									message="Players' Names Are Required"
+									type="warning"
+									showIcon
+									closable
+									onClick={() => {
+										setShowAlert(false);
+									}}
+									style={{}}
+								/>
+							</Space>
+						)}
+					</div>
+					{isEnabled && (
+						<Quiz
+							setIsFinished={setIsFinished}
+							setIsWinner={setIsWinner}
+							setOneScore={setOneScore}
+							oneScore={oneScore}
+							setTwoScore={setTwoScore}
+							twoScore={twoScore}
+							enabled={isEnabled}
+							setEnabled={setIsEnabled}
+							setCurrentQuestionIndex={setCurrentQuestionIndex}
+							currentQuestionIndex={currentQuestionIndex}
+							fPlayerName={fPlayerName}
+							sPlayerName={sPlayerName}
+						/>
+					)}
+				</div>
+				<div
+					className={
+						activePlayer === 2 ? styles.activePlayerSide : styles.playerSide
+					}
+				>
+					<div className={styles.playerName}>
+						<div
+							className={
+								isWinner === 2 && isWinner !== 0 && isWinner !== 1
+									? styles.secondTrophy
+									: styles.hide
+							}
+						>
+							{isWinner && winAudio()} <TrophyFilled />
+						</div>
+						{activePlayer === 2 && (
+							<p
+								style={{
+									display: "flex",
+									position: "absolute",
+									top: "36%",
+									right: "17%",
+								}}
+							></p>
+						)}
+						<h2> {sPlayerName} </h2>
+					</div>
+					<div className={styles.score}>{twoScore}</div>
+				</div>
+			</div>
+			<InitialModal />
+		</>
+	);
 }
 export default MainPage;
